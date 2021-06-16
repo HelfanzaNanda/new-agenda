bb995fb36178865bfc3ecc8493297689055e1b62
HelfanzaNanda <elvannnd@gmail.com>
2021-06-16 16:36:13 +0700

    dashboard

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -22,7 +22,8 @@ class DashboardController extends Controller
 			array_push($dates, $date->translatedFormat('d F Y'));
 		}
 		$columns = [
-			'file'	=> '<th>File</th>',
+			'file_materi'	=> '<th>File Materi</th>',
+			'file_undangan'	=> '<th>File Undangan</th>',
 			'jam'	=> '<th>Jam</th>',
 			'kegiatan'	=> '<th>Agenda</th>',
 			'tempat'	=> '<th>Tempat</th>',
@@ -40,11 +41,20 @@ class DashboardController extends Controller
 		$users = Agenda::whereDate('tanggal_mulai', $this->convertDate($request->date))->get();
         $datatables = datatables($users)
 		->addIndexColumn()
-		->addColumn('file', function($row){
+		->addColumn('file_materi', function($row){
 			$btn = '<form action="'.route('agenda.download').'" class="d-inline mr-2" target="_blank" method="POST">';
 			$btn .= ' ' . csrf_field().' ';
 			$btn .= '	<input type="hidden" value="'.$row->materi.'" name="file">';
-			$btn .= '	<button class="btn btn-primary btn-sm text-white">Download Materi</button>';
+			$btn .= '	<button class="btn btn-primary btn-sm text-white">Download</button>';
+			$btn .= '</form>';
+			return $btn;
+		})
+
+		->addColumn('file_undangan', function($row){
+			$btn = '<form action="'.route('agenda.download').'" class="d-inline mr-2" target="_blank" method="POST">';
+			$btn .= ' ' . csrf_field().' ';
+			$btn .= '	<input type="hidden" value="'.$row->undangan.'" name="file">';
+			$btn .= '	<button class="btn btn-primary btn-sm text-white">Download</button>';
 			$btn .= '</form>';
 			return $btn;
 		})
@@ -55,7 +65,7 @@ class DashboardController extends Controller
 		->addColumn('jam', function($row){
 			return $row->jam_mulai . ' - ' . ($row->jam_selesai ?? 'Selesai');
 		})
-		->rawColumns(['file']);
+		->rawColumns(['file_materi', 'file_undangan']);
         return $datatables->toJson();
 	}
 
